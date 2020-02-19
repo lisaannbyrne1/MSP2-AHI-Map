68582dfc1c81a2be1222f43f2b3a255753611b75
Lisa Byrne <lisaannbyrne1@hotmail.com>
2020-02-19 18:20:25 +0000

    Zoom to county, results and clear results

--- assets/js/map.js
+++ assets/js/map.js
@@ -858,34 +858,82 @@ function initialize() {
 
     map = new google.maps.Map(document.getElementById('map'), options);
 
+    var countyselect = document.getElementById('county');
+    countyselect.addEventListener('change', onCountySelect);
+    renderMarkers();
 
-    //Create markers
-    let markers = [];
+}
 
-    function renderMarkers(showInfoWindow) {
-        for (var i = 0; i < practices.length; i++) {
-            markers[i] = new google.maps.Marker({
-                position: practices[i].position,
-                title: practices[i].name,
-                map: map,
-                animation: google.maps.Animation.DROP,
-            });
+//Create markers
+let markers = [];
 
-            //Show inforwindow
+function renderMarkers(showInfoWindow) {
+    for (var i = 0; i < practices.length; i++) {
+        markers[i] = new google.maps.Marker({
+            position: practices[i].position,
+            title: practices[i].name,
+            map: map,
+            animation: google.maps.Animation.DROP,
+        });
 
-            google.maps.event.addListener(markers[i], 'click', function() {
-                console.log(this)
-                let infoWindow = new google.maps.InfoWindow();
-                infoWindow.setContent(this.getTitle());
-                infoWindow.open(map, this);
-            });
-        }
+        //Show inforwindow
+
+        google.maps.event.addListener(markers[i], 'click', function() {
+            console.log(this)
+            let infoWindow = new google.maps.InfoWindow();
+            infoWindow.setContent(this.getTitle());
+            infoWindow.open(map, this);
+        });
+    }
+
+}
+
+
+//Zoom to selected county
+function onCountySelect() {
+    var countySelect = document.getElementById("county");
+    var countiesByCode = counties.map(function(c) {
+        return c.code;
+    });
+    var index = countiesByCode.indexOf(countySelect.value)
+
+    if (index > -1) {
+        var selectedCounty = counties[index];
+        map.setCenter(selectedCounty.center);
+        map.setZoom(selectedCounty.zoom);
+
+        renderResultsList(selectedCounty);
+    } else {
+        map.setCenter({
+            lat: 53.2734,
+            lng: -7.77832031
+        });
+        map.setZoom(6.5);
+
+        clearResultsList();
+    }
+}
+
+//Show results by county
+function renderResultsList(selectedCounty) {
+    var relevantPractices = practices.filter(function(p) {
+        return p.code === selectedCounty.code;
+    });
+    var resultsArea = document.getElementById("results");
+    resultsArea.innerHTML = "";
+
+    for (var i = 0; i < relevantPractices.length; i++) {
+        var practice = relevantPractices[i];
+        var div = document.createElement("div");
+        div.className = "result";
+        div.innerHTML = `<h5>${practice.name}</h5><p>${practice.address}</p><p>${practice.vet}</p>`;
+        resultsArea.appendChild(div);
 
     }
 }
 
-//Zoom to selected county
-
-//Show results by county
-
 //Reset results
+
+function clearResultsList() {
+    document.getElementById("results").innerHTML = "";
+}
